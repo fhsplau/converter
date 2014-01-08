a625b38fb7a07ed48983c0c4cfb88461aa9c6f44
paverell <piotr.kacprzak11@gmail.com>
2014-01-08 19:44:31 +0000

    New Dec2BinU2

--- src/main/java/com/mylib/converter/keywords/Dec2BinU2.java
+++ src/main/java/com/mylib/converter/keywords/Dec2BinU2.java
@@ -1,34 +1,51 @@
 package com.mylib.converter.keywords;
 
+import org.robotframework.javalib.annotation.ArgumentNames;
+import org.robotframework.javalib.annotation.RobotKeyword;
+import org.robotframework.javalib.annotation.RobotKeywordOverload;
+import org.robotframework.javalib.annotation.RobotKeywords;
+
 /**
- * Created by paverell on 1/3/14.
+ * Created by kacprzakp on 1/3/14.
  */
+
+@RobotKeywords
 public class Dec2BinU2 extends Dec2Bin{
 
-    public Dec2BinU2(){
-        super();
-    }
+    private static final int numberOfBitsForSign = 1;
+    private int sign = 0;
 
-    public String dec2binu2(int dec, int numberOfBits) {
+    @RobotKeyword
+    @ArgumentNames({"decimalValue","numberOfBits="})
+    public String dec2binU2(int dec, int numberOfBits) {
+        String bin;
         StringBuilder binU2 = new StringBuilder();
-        int minNumberOfBits = this.minimumNumberOfBits(dec);
+        int numberOfExtraBits;
+        int minNumberOfBits = minimumNumberOfBits(dec) + numberOfBitsForSign;
+
         properNumberOfBits(numberOfBits, minNumberOfBits);
-        if (dec >= 0){
-            if (dec == 0 || dec == 1){
-                return binU2.append(extraBits(numberOfBits)).append(dec).toString();
-            }
+        setSign(dec);
 
-        }
-        else{
-            if (dec == -1)
-                return binU2.append(extraBits(numberOfBits)).toString();
+        bin = dec2binU2Conversion(dec, numberOfBits);
+        numberOfExtraBits = numberOfBits - bin.length() - 1;
 
-        }
-        return binU2.append("0000").toString();
+        return binU2.
+                append(sign).
+                append(extraBits(numberOfExtraBits)).
+                append(bin).
+                toString();
     }
 
-    @Override
-    protected int minimumNumberOfBits(int dec){
-        return super.minimumNumberOfBits(dec)+1;
+    private String dec2binU2Conversion(int dec, int numberOfBits) {
+        if (sign == 0)
+            return  dec2bin(dec);
+        else
+            return  dec2bin(enableMostSignificantBit(numberOfBits) + dec);
+    }
+
+
+    private void setSign(int dec){
+        if (dec <0)
+            sign = 1;
     }
 }
